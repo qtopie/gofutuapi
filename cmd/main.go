ccb8ee7de8e908027ebd1c67b08d6bd6514c54e5
qtopie.rw <qtopie.rw@gmail.com>
2025-07-28 23:26:36 +0800

    {commit_message}

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello")
+	// Connect to a TCP server (e.g., localhost:8080)
+	conn, err := net.DialTimeout("tcp", "localhost:11111", 5*time.Second)
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close() // Ensure the connection is closed when done
+
+	fmt.Println("Connected to TCP server!")
+
+	// You can now send and receive data using conn.Write() and conn.Read()
+	// For example, sending a message:
+	_, err = conn.Write([]byte("Hello from Go client!"))
+	if err != nil {
+		log.Printf("Failed to send data: %v", err)
+	}
 }
